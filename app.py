89e5e582ecaa4e6628b10e05f6f1b567ef22bbda
924255955 <95343075+924255955@users.noreply.github.com>
2022-09-27 00:42:52 +0700

    Init
    2022-09-27 00:42

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, flash
+
+a = 10
+
+app = Flask(__name__)
+app.secret_key = "1234abcd"
+
+@app.route("/hello")
+def index():
+    flash("คุณชื่ออะไรหรือครับ?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("สวัสดีครับ คุณ " + str(request.form['name_input']) + ", ดีใจที่รู้จักครับ")
+    return render_template("index.html")
+    
